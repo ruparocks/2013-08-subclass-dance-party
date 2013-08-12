f6de557ac819d698c19e475650fedcac8dd880b0
Hack Reactor Students <github@hackreactor.com>
2013-08-12 14:57:33 -0700

    Started to modify Dancer class and the blinkDancer to convert to pseudoclassical.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,6 +1,5 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
+  var blinkyDancer = new Dancer(top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   var oldStep = blinkyDancer.step;
