ad6c1a6499d1e811c4465ec123b36049cc3005d9
Hack Reactor Students <github@hackreactor.com>
2013-08-13 10:04:20 -0700

    Renamed ShakeyDancer to ShakyDancer

--- src/shakyDancer.js
+++ src/shakyDancer.js
@@ -1,4 +1,4 @@
-var ShakeyDancer = function(top, left, timeBetweenSteps){
+var ShakyDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this,top,left,timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
@@ -10,15 +10,16 @@ var ShakeyDancer = function(top, left, timeBetweenSteps){
   this.newLeft = left || 0;
 };
 
-ShakeyDancer.prototype = new Dancer();
-ShakeyDancer.prototype.constructor = ShakeyDancer;
-ShakeyDancer.prototype.step = function(){
+ShakyDancer.prototype = new Dancer();
+ShakyDancer.prototype.constructor = ShakyDancer;
+ShakyDancer.prototype.step = function(){
     // call the old version of step at the beginning of any call to this new version of step
     Dancer.prototype.step.call(this);
 
     /* toggle() is a jQuery method to show/hide the <span> tag.
      * See http://api.jquery.com/category/effects/ for this and
      * other effects you can use on a jQuery-wrapped html tag. */
+     // 0-255, 0-255, 0-255
     this.addTop ? this.newTop = this.newTop + (10 * Math.random()) : this.newTop = this.newTop - (10 * Math.random());
     this.addLeft ? this.newLeft = this.newLeft + (10 * Math.random()) : this.newLeft = this.newLeft - (10 * Math.random());
     this.addTop ? this.addTop = false : this.addTop = true;
